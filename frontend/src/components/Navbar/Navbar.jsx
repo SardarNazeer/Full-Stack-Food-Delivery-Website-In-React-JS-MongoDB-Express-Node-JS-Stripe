49d3b667c0ea98d15176e017490b7b736475635b
Sardar Nazeer <sardarnazeer1108@gmail.com>
2025-10-21 01:58:22 +0500

    Small frontend updates:

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -4,16 +4,16 @@ import { assets } from '../../assets/assets';
 
 const Navbar = () => {
 
-  const [menu,setMenu] = useState("home");
+  const [menu,setMenu] = useState("menu");
 
   return (
     <div className='navbar'>
       <img src={assets.logo} alt="" className='logo'/>
       <ul className="navbar-menu">
-        <li className={menu==="home"?"active":""}>Home</li>
-        <li className={menu==="menu"?"active":""}>Menu</li>
-        <li className={menu==="mobile-app"?"active":""}>Mobile-App</li>
-        <li className={menu==="contact-us"?"active":""}>Contact Us</li>
+        <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Home</li>
+        <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>Menu</li>
+        <li onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>Mobile-App</li>
+        <li onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>Contact Us</li>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
